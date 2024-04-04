375c050792da7bb2dda1cc655e690b1a7236f75b
gnivet <guillaume.nivet@etik.com>
2024-04-04 15:38:24 +0200

    Mise à jour

--- js/meteo.js
+++ js/meteo.js
@@ -6,6 +6,7 @@ const searchButton = document.getElementById('searchButton');
 const apiInput = document.getElementById('apiInput');
 const apiButton = document.getElementById('apiButton');
 const locationElement = document.getElementById('location');
+const locationConstElement = document.getElementById('locationConst');
 const temperatureElement = document.getElementById('temperature');
 const descriptionElement = document.getElementById('description');
 const latitudeElement = document.getElementById('latitude');
@@ -18,25 +19,27 @@ const temp_maxElement = document.getElementById('temp_max');
 const windElement = document.getElementById('wind');
 const feels_likeElement = document.getElementById('feels_like');
 
-
-
-
 searchButton.addEventListener('click', () => {
-    const location = locationInput.value;
+    let location = locationInput.value;
     if (location) {
         fetchWeather(location);
     }  
+    
 });
 
 apiButton.addEventListener('click', () => {
-          
+    let location = 'Cognac';      
         
-        requestApi();
+    if (location)
+    {
+        fetchWeather(location);
+        locationInput.textContent = `${(location)}`;
+    }
       
 });
 
 function fetchWeather(location) {
-    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+   let url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
 
     fetch(url)
         .then(response => response.json())
@@ -44,6 +47,16 @@ function fetchWeather(location) {
             locationElement.textContent = data.name;
             temperatureElement.textContent = `${Math.round(data.main.temp)} °C`;
             descriptionElement.textContent = data.weather[0].description;
+            feels_likeElement.textContent = `Feels like:${Math.round(data.main.feels_like)}`;         
+            temperatureElement.textContent = `Temp :${Math.round(data.main.temp)} °C`;         
+            longitudeElement.textContent = `Longitude: ${(data.coord.lon)}`;
+            latitudeElement.textContent = `Latitude: ${(data.coord.lat)}`;   
+            pressureElement.textContent = `Pressure: ${(data.main.pressure)}`;  
+            humidityElement.textContent = `Humidity: ${(data.main.humidity)}`;  
+            temp_minElement.textContent = `Temp min: ${(data.main.temp_min)} °C`; 
+            temp_maxElement.textContent = `Temp max: ${(data.main.temp_max)} °C`;  
+            windElement.textContent = `Wind: ${(data.wind.speed)} Milles/heure`;
+                                  
         })
         .catch(error => {
             console.error('Error fetching weather data:', error);
@@ -51,27 +64,5 @@ function fetchWeather(location) {
 };
 
 
-function requestApi(){
-    const apiUrl2 = 'https://api.openweathermap.org/data/2.5/weather?lat=45.7&lon=-0.3333&units=5&appid=5f1b9d82972923335a93993455857f3a'; 
-      
-       fetch(apiUrl2)
-        .then(response => response.json())
-        .then(data2 => {
-            cityElement.textContent = data2.name;  
-            descriptionElement.textContent = data2.weather[0].description;   
-            feels_likeElement.textContent = `Feels like:${Math.round(data2.main.feels_like)}`;         
-            temperatureElement.textContent = `Temp :${Math.round(data2.main.temp)} °C`;         
-            longitudeElement.textContent = `Longitude: ${(data2.coord.lon)}`;
-            latitudeElement.textContent = `Latitude: ${(data2.coord.lat)}`;   
-            pressureElement.textContent = `Pressure: ${(data2.main.pressure)}`;  
-            humidityElement.textContent = `Humidity: ${(data2.main.humidity)}`;  
-            temp_minElement.textContent = `Temp min: ${(data2.main.temp_min)} °C`; 
-            temp_maxElement.textContent = `Temp max: ${(data2.main.temp_max)} °C`;  
-            windElement.textContent = `Wind: ${(data2.wind.speed)} Milles/heure`;
-            
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-        }); 
-}; 
+
 
